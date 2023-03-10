d7ab0b0e75119226ab4f4f6f91944a414899759f
Alejandro Jimenez <ajimenezal01@gmail.com>
2023-03-10 10:22:30 +0100

    Entrega Ejercicios

---
+++ ClaseInteger/src/ClaseInteger.java
@@ -0,0 +1,23 @@
+
+public class ClaseInteger {
+
+	public static void main(String args []) {
+		//Creación de un objeto integer usando el segundo constructor 
+		Integer num1=12;
+		//Creación de un objeto Integer usando el primer constructor
+		Integer num2 = 20;
+		int n1=num1.intValue();
+		int n2=num2.intValue();
+		System.out.println("Suma de "+n1+" y "+n2+" vale "+(n1+n2)); 
+		if(n1+n2 > 130) {
+			n1++;
+		}
+			else {
+				n1--;
+				System.out.println(n1);
+			}			
+		}
+		
+	}
+	
+
